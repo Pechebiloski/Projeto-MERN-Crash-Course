c119cab76c513eeb1a04d82b1d4e9ad736154110
Pechebiloski <devpechebiloski@gmail.com>
2026-09-06 10:57:20 -0300

    feat: add MongoDB connection and flash model with CRUD endpoint

---
+++ backend/models/flashs.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const flashSchema = new mongoose.Schema(
+    {
+    name: {
+        type: String,
+        required: true,
+    },
+
+    price: { 
+    type: Number,
+    required: true,
+}, 
+    img: { 
+    type: String,
+    required: true, 
+    },
+
+    },
+
+    {
+        timestamps: true,
+    },
+);
+
+const flashs = mongoose.model("flashs", flashSchema);
+
+export default flashs;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,38 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import dotenv from "dotenv"
+import { connectDB } from './config/db.js';
+import flashs from './models/flashs.model.js';
+
+
+dotenv.config();
 
 const app = express();
 
+app.post("/flashs", async (request, response) => {
+    const flash = request.body;
+
+    if(!flash.name || !flash.price || flash.image) {
+        return response.status(400).json({ success: false, message: "please provide all fields"});
+    }
+
+    const newFlash = new flash(flash);
+
+    try {
+        await newFlash.save();
+
+        response.status(201).json({ success: true, data: newFlash});
+    
+    } catch (error) {
+        console.error("error in create flash:", error.message);
+        response.status(500).json({ success: false, message: "server error" });
+
+    }
+});
+
+console.log(process.env.MONGO_URI);
+
+
 app.listen(5000, () => {
-    console.log("server started at http://localhost:5000 hello");
+    connectDB();
+    console.log("server started at http://localhost:5000");
 });
