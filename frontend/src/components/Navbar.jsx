136207417568b395d6a718d369738dfc0563716a
Pechebiloski <devpechebiloski@gmail.com>
2026-09-12 18:16:19 -0300

    feat: atualizar estrutura do Navbar e implementar página de criação de flashs

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,49 +1,55 @@
-import { Container, Flex, Text, Link, HStack, Button } from "@chakra-ui/react";
-import { PlusSquareIcon } from "@chakra-ui/icons";
+import { Button, Container, Flex, HStack, Text, } from "@chakra-ui/react";
+
+import { Link } from "react-router-dom";
+
+import { FaPenNib } from "react-icons/fa";
 import { IoMoon } from "react-icons/io5";
 import { LuSun } from "react-icons/lu";
 
-
+import { useColorMode } from "./ui/color-mode";
 
 const Navbar = () => {
-    const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
 
-    return (
-      <Container maxW={"1140px"} px= {4}>
-        <Flex
-        h = {16}
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
         alignItems={"center"}
         justifyContent={"space-between"}
-        flexDirection={{
-            base: "column",
-            sm: "row"
+        flexDir={{
+          base: "column",
+          sm: "row",
         }}
-        >
-            <Text
-					fontSize={{ base: "22", sm: "28" }}
-					fontWeight={"bold"}
-					textTransform={"uppercase"}
-					textAlign={"center"}
-					 color={"#F5F0E6"}
-				>
-					<Link to={"/"}>Flashs Disponiveis</Link>
-				</Text>
+      >
+        <Text
+          fontSize={{ base: "22px", sm: "28px" }}
+          fontWeight="bold"
+          textTransform="uppercase"
+          textAlign="center"
+          >
+            <Link
+              to="/"
+              style={{ color: "inherit", textDecoration: "none" }}
+            >
+              Flashs Disponíveis
+             </Link>
+        </Text>
 
-            <HStack spacing={2} alignItems={"center"}>
-            <Link to={"/CreatePage"}>
+        <HStack gap={2} alignItems={"center"}>
+          <Link to={"/create"}>
             <Button>
-               	<PlusSquareIcon fontSize={20} />
+              <FaPenNib size={20} />
             </Button>
-            </Link>
-            <Button onClick={toggleColorMode}>
-						{ colorMode === "light" ? <IoMoon /> : <LuSun size='20' />}
-					</Button>
+          </Link>
 
-            </HStack>
-        </Flex>
-
-      </Container>
-    )
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoon /> : <LuSun size={20} />}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
 };
 
-export default Navbar
+export default Navbar;
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,91 @@
+import { Box, Button, Container, Heading, Input, VStack, } from "@chakra-ui/react";
+
+import { useState } from "react";
+
 const CreatePage = () => {
-    return (
-        <div>CreatePage</div>
-    )
+  const [newFlash, setNewFlash] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const handleAddFlash = async () => {
+    console.log(newFlash);
+  };
+
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack gap={8}>
+        <Heading
+          as={"h1"}
+          size={"2xl"}
+          textAlign={"center"}
+          mb={8}
+        >
+          Cadastrar novo flash
+        </Heading>
+
+        <Box
+          w={"full"}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack gap={4}>
+            <Input
+              placeholder="Nome do flash"
+              name="name"
+              value={newFlash.name}
+              onChange={(e) =>
+                setNewFlash({
+                  ...newFlash,
+                  name: e.target.value,
+                })
+              }
+            />
+
+            <Input
+              placeholder="Preço"
+              name="price"
+              type="number"
+               min={0}
+              value={newFlash.price}
+              onChange={(e) => {
+                const value = e.target.value;
+
+                 if (value === "" || Number(value) >= 0) {
+                    setNewFlash({
+                    ...newFlash,
+                     price: value,
+                });
+              }
+              }
+             }
+            />
+
+            <Input
+              placeholder="URL da imagem"
+              name="image"
+              value={newFlash.image}
+              onChange={(e) =>
+                setNewFlash({
+                  ...newFlash,
+                  image: e.target.value,
+                })
+              }
+            />
+
+            <Button
+              onClick={handleAddFlash}
+              w={"full"}
+            >
+              Cadastrar flash
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
 };
 
-export default CreatePage
+export default CreatePage;
